1f5d93c2bc2b38966149c708c46ede363a417066
David Mecalco <dmecalcoa1200@alumno.ipn>
2022-07-07 23:21:37 -0500

    Inicializamos el repositorio para le curso

---
+++ src/PrimitivosEnteros.java
@@ -0,0 +1,19 @@
+public class PrimitivosEnteros {
+    public static void main(String[] args) {
+        byte num1 = 1;
+        System.out.println("num1 = " + num1);
+        System.out.println("Un byte en bYts es = " + Byte.BYTES);
+        System.out.println("Un byte en bYts es = " + Byte.SIZE);
+        System.out.println("El número máximo de byte es = " + Byte.MAX_VALUE);
+        System.out.println("El número máximo de byte es = " + Byte.MIN_VALUE);
+
+        Short num2 = 34;
+        System.out.println("num2 = " + num2);
+        System.out.println("Un short en byts es:  = " + Short.SIZE);
+        System.out.println("Un short en bits es = " + Short.BYTES);
+        System.out.println("El número máximo para un short es: " + Short.MAX_VALUE);
+        System.out.println("El número minimo para un short es: " + Short.MIN_VALUE);
+
+
+    }
+}
