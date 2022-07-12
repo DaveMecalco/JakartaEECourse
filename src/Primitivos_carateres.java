9691d493caa07a954c2fb2a5b480de6638382f47
David Mecalco <dmecalcoa1200@alumno.ipn>
2022-07-12 13:32:30 -0500

    Agregamos las clases de Caracteres y la clase inicial Hola Munod

---
+++ src/Primitivos_carateres.java
@@ -0,0 +1,17 @@
+public class Primitivos_carateres {
+    public static void main(String[] args) {
+        char caracter1 = 'a';
+        System.out.println("caracter1 = " + caracter1);
+
+        //caracter en unicode
+        char ucaracter = '\u0040';
+        System.out.println("Char u0040 en unicode en es: " +ucaracter);
+
+        char caracter_especial = '@';
+        System.out.println("caracter_especial es = " + caracter_especial);
+
+        char caracter_decimal = 64;
+        System.out.println("caracter_decimal 64 es = " + caracter_decimal);
+
+    }
+}
