9174127ff32c50bfa1173c6a03a60b76a9183837
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-07-25 15:54:00 -0500

    Agregamos la clase EjemploStringMetodoArreglos

---
+++ src/Section2/EjemploStringMetodoArreglos.java
@@ -0,0 +1,13 @@
+package Section2;
+
+public class EjemploStringMetodoArreglos {
+    public static void main(String[] args) {
+
+        //Declaramos el String
+        String trabalenguas = "trabalenguas";
+
+        //Imprimimos el String trabalenguas
+        System.out.println("trabalenguas = " + trabalenguas);
+
+    }
+}
