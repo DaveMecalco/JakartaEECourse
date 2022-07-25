6ec8d75891d89587a0d792fe12a9ea12b445c5d6
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-07-25 00:11:28 -0500

    Agregamos la clase EjemploStringExtencionesArchivos

---
+++ src/Ejercicios/EjemploStringExtensionesArchivos.java
@@ -0,0 +1,7 @@
+package Ejercicios;
+
+public class EjemploStringExtensionesArchivos {
+    public static void main(String[] args) {
+        //Poder arreglar una cade de caracteres de un archivo con cualquier terminación, aplicando los diferentes métodos existentes vistos en la sección 2
+    }
+}
