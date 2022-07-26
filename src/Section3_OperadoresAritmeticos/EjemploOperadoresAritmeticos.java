c0dfcb66cbab3c4d768cb3061c4bbc3381d6d3a0
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-07-26 16:08:30 -0500

    Cambios los nombres de los paquetes

--- src/Section3_OperadoresAritmeticos/EjemploOperadoresAritmeticos.java
+++ src/Section3_OperadoresAritmeticos/EjemploOperadoresAritmeticos.java
@@ -3,6 +3,5 @@ package Section3_OperadoresAritmeticos;
 public class EjemploOperadoresAritmeticos {
     public static void main(String[] args) {
         //Inicializamos la clase
-        
     }
 }
