45390a2c92f2a5ae7b9e654764022e4db4592650
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-08-06 21:58:52 -0500

    Modificamos la clase OperadorInstaceOf v3

--- src/Section3_OperadoresAritmeticos/OperadorInstanceof.java
+++ src/Section3_OperadoresAritmeticos/OperadorInstanceof.java
@@ -22,6 +22,33 @@ public class OperadorInstanceof {
         boolean b5 = numero instanceof Object;
         System.out.println("b5 = " + b5);
 
+        //Si definimos una varibale con Object en java, nos permite comparar instancias con instance of con cualquier tipo de Primitivo
+
+        Object var1 = "Esta es una variable con Object";
+        boolean b6 = var1 instanceof String;
+        System.out.println("b6 = " + b6);
+
+        boolean b7 = var1 instanceof Integer;
+        System.out.println("b7 = " + b7);
+
+        boolean b8 = var1 instanceof Double;
+        System.out.println("b8 = " + b8);
+
+        boolean b9 = var1 instanceof Float;
+        System.out.println("b9 = " + b9);
+
+        //Para realziar una instancia de Integer
+        Integer num2 = Integer.valueOf(9);
+
+        boolean b10 = num2 instanceof Integer;
+        System.out.println("b10 = " + b10);
+        
+        Double num3 = Double.valueOf(5.6);
+        
+        boolean b11 = num3 instanceof Double;
+        System.out.println("b11 = " + b11);
+
+
         
     }
 }
