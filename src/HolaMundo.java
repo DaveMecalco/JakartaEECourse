1a8f363ac55aeba9fd1f7e9f7103883ac80d0605
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-08-06 22:07:19 -0500

    Agregamos la clase PrecedenciaOperadores

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,9 @@
+import javax.swing.*;
+
 public class HolaMundo {
     public static void main(String[] args) {
 
         //Imprimir mensajes en consolo con la Clase System
-        System.out.println("Hola Mundo desde Java para el repositorio de git");
+        JOptionPane.showMessageDialog(null, "Hola, bievenido al repositorio de Java");
     }
 }
---
+++ src/Section3_OperadoresAritmeticos/PrecedenciaOperadores.java
@@ -0,0 +1,15 @@
+package Section3_OperadoresAritmeticos;
+
+public class PrecedenciaOperadores {
+    public static void main(String[] args) {
+        int i = 14;
+        int j = 8;
+        int k = 20;
+
+        //Siempre utilizar parentesis para realizar la precedencia de los operadores adecuadamente
+        double promedio = (i + j + k) / 3d;
+        System.out.println(promedio);
+
+        
+    }
+}
