83108b57c2cb3d2a14c85e291cb7fda2e101f0e3
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-08-09 11:12:03 -0500

    Agregamos la clase SentenciaFor

---
+++ src/Section4_FlujosControl/SentenciaFor.java
@@ -0,0 +1,41 @@
+package Section4_FlujosControl;
+
+public class SentenciaFor {
+    public static void main(String[] args) {
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println("i = " + i);
+        }
+
+        for (int j = 10; j >= 0; j--){
+            System.out.println("j = " + j);
+        }
+
+        //Imprimir números impares
+
+        for (int i = 0; i <= 10; i++){
+            if(i % 2 ==0){
+                //Continue se utiliza para que no salga del bucle el for al iterar, entonces solo esca la sentencia if
+                continue;
+            }
+            System.out.println("i = " + i);
+        }
+
+        //Imprimir números pares
+
+        for (int i = 0; i <= 10; i++){
+            if(i % 2 !=0){
+                continue;
+            }
+            System.out.println("i = " + i);
+        }
+
+        //Bucles sobre strings
+        String[] nombres = {"David", "Anahi", "Paco", "Saul", "Pepe", "José", "Clara", "Maria", "Ismael", "Juan", "Pedro", "Bea", "Clara"};
+        int conunt = nombres.length;
+        for (int i = 0; i < conunt; i++){
+            System.out.println(i + " - " + nombres[i]);
+        }
+
+    }
+}
