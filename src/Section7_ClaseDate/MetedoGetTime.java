fc8deba89af77b8323c6a9e27e9e757801db558f
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-08-29 23:17:02 -0500

    Agregamos la clase MetodoGetTime

---
+++ src/Section7_ClaseDate/MetedoGetTime.java
@@ -0,0 +1,36 @@
+package Section7_ClaseDate;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MetedoGetTime {
+
+    public static void main(String[] args) {
+
+        //Inicializamos el paquete Date
+
+        Date fecha =  new Date();
+        System.out.println(fecha);
+
+        //Damos formato a la fecha con la el constructor SimpleDateFormat
+        SimpleDateFormat df = new SimpleDateFormat("EEEE dd 'de' MMMM");
+
+        //Imprimimos el formato de fecha asignándola a una variable
+        String fechaStr = df.format(fecha);
+        System.out.println("fechaStr = " + fechaStr);
+
+        int j = 0;
+        for (int i = 0; i < 1000000000; i++){
+            j += 1;
+        }
+
+        System.out.println("j = " + j);
+
+        Date fecha2 = new Date();
+        long tiempoFinal = fecha2.getTime() - fecha.getTime();
+
+        System.out.println("Tiempo transcurrido en el for = " + tiempoFinal);
+        System.out.println("fechaStr = " + fechaStr);
+    }
+
+}
