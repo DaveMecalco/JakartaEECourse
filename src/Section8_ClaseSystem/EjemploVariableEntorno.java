ac9c415590c10732607d6eacb4289fdf5808a644
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-09-19 22:39:01 -0500

    Agregamos la Clase EjemploVariableEntorno

---
+++ src/Section8_ClaseSystem/EjemploVariableEntorno.java
@@ -0,0 +1,34 @@
+package Section8_ClaseSystem;
+
+import java.util.Map;
+
+public class EjemploVariableEntorno {
+    public static void main(String[] args) {
+
+        //Para desplegar las variables de ambiente del sistema utilizamos Map y le asignamos System.getenv
+        Map<String, String > varEnv = System.getenv();
+        System.out.println("Variables de ambiente del sistema: " + varEnv);
+
+        //Pasar una variable de ambiente por argumento
+        String userName = System.getenv("USERNAME");
+        System.out.println("userName = " + userName);
+
+        //Variable JavaHome
+        String javaHome = System.getenv("JAVA_HOME");
+        System.out.println("javaHome = " + javaHome);
+
+        //Variable Path
+        String path = System.getenv("PATH");
+        System.out.println("path = " + path);
+
+        //Variable Temp
+        String temp = System.getenv("Temp");
+        System.out.println("temp = " + temp);
+
+        //Método 2 para desplegar una variable de entorno
+        String path2 = varEnv.get("PATH");
+        System.out.println("path2 = " + path2);
+
+
+    }
+}
