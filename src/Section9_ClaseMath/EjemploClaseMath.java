f477ac7c658651afb1ffd9ca60af5b89a05facd7
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-09-26 12:53:00 -0500

    Agregamos el paquete Section9_ClaseMath

---
+++ src/Section9_ClaseMath/EjemploClaseMath.java
@@ -0,0 +1,16 @@
+package Section9_ClaseMath;
+
+public class EjemploClaseMath {
+    public static void main(String[] args) {
+
+        //Valor absoluto
+        int valor1 = Math.abs(-45);
+        System.out.println("valor1 = " + valor1);
+
+        int valor2 = Math.abs(84);
+        System.out.println("valor2 = " + valor2);
+
+        
+
+    }
+}
