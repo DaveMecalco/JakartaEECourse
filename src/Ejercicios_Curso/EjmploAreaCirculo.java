2a1c652875d85317b3d91be4c98ef32280a722a8
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-09-27 18:52:32 -0500

    Agregamos la clase EjemploAreaCirculo

---
+++ src/Ejercicios_Curso/EjmploAreaCirculo.java
@@ -0,0 +1,22 @@
+package Ejercicios_Curso;
+import java.util.Scanner;
+
+public class EjmploAreaCirculo {
+    public static void main(String[] args) {
+
+        //Objeto scanner para ingresar el radio
+        Scanner scanner = new Scanner(System.in);
+        //Instrucción al sistema
+        System.out.println("Ingrese el radio del circulo: ");
+        //Guardamos el radio en la variable r
+        String r = scanner.nextLine();
+        //Casteamos el radio de string a double
+        double radio = Double.parseDouble(r);
+        //Calculamos el área = PI*r^2
+        double area = Math.PI * Math.pow(radio, 2);
+        //Imprimimos nuestro resultado
+        System.out.println("El área del circulo es: " + area);
+
+
+    }
+}
