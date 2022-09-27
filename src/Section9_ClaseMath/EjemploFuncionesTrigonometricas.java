d1132f40d79363a25f7889f5e67d8c7bed2f4a63
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-09-27 17:46:54 -0500

    Agregamos la clase EjemploFuncionesTringonemetricas

--- src/Section9_ClaseMath/EjemploFuncionesTrigonometricas.java
+++ src/Section9_ClaseMath/EjemploFuncionesTrigonometricas.java
@@ -3,7 +3,13 @@ package Section9_ClaseMath;
 public class EjemploFuncionesTrigonometricas {
     public static void main(String[] args) {
 
-        //Función seno
+        //Función seno, solo recibe radianes
+        double funcioseno = Math.sin(1.5);
+        System.out.println("funcioseno = " + funcioseno);
+
+        //función coseno, también recibe el parametro en radianes
+        double funsioncoseno = Math.cos(0.00);
+        System.out.println("funsioncoseno = " + funsioncoseno);
         
 
     }
