d17e21efa2074c720600892ba7476229cfc71add
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-09-30 16:51:14 -0500

    Agregamos el paquete Section10_Terminal y la clase EjemploTerminal

---
+++ src/Section10_Terminal/EjemploTerminal.java
@@ -0,0 +1,20 @@
+package Section10_Terminal;
+
+public class EjemploTerminal {
+    public static void main(String[] args) {
+
+        //Para compilar y ejecutar mediante la linea de comandos debemos utilizar javac para generar el archivo .class (Bytecode)
+
+        if(args.length == 0){
+            //El exit -1 es un error de estado
+            //El exit 1 es que se ha compilado bien y ejecutado
+            System.exit(-1);
+        }
+        for (int i = 0; i < args.length; i++){
+            System.out.println("Argumentos nro " + i + " : " + args[i]);
+        }
+
+        //Después de ejecutar el comando javac (Java compiler) debemos utilizar java y el nombre del archivo
+
+    }
+}
