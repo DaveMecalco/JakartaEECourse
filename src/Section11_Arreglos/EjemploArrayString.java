8750f4a171fd8a416f1a76e2839f71adfe3ce7fb
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-10-03 20:11:25 -0500

    Modifcamos la clase EjemploArrayString

--- src/Section11_Arreglos/EjemploArrayString.java
+++ src/Section11_Arreglos/EjemploArrayString.java
@@ -1,16 +1,13 @@
 package Section11_Arreglos;
 
+import java.util.Arrays;
+
 public class EjemploArrayString {
     public static void main(String[] args) {
 
         //Al instance los arreglos String, su valor asignado por defecto será null
 
         String[] arregloStrin = new String[7];
-        System.out.println("arregloStrin = " + arregloStrin[0]);
-        System.out.println("arregloStrin = " + arregloStrin[1]);
-        System.out.println("arregloStrin = " + arregloStrin[2]);
-        System.out.println("arregloStrin = " + arregloStrin[3]);
-        System.out.println("arregloStrin = " + arregloStrin[4]);
 
         //Asignar valores al array
         arregloStrin[0] = "Kingston Pendrive 64GB";
@@ -20,6 +17,36 @@ public class EjemploArrayString {
         arregloStrin[4] = "Macbook air";
         arregloStrin[5] = "Amazon Alexa";
         arregloStrin[6] = "Bicicleta Oxford";
+
+        //Ordenar de manera alphabetical  un arreglo mediante la clase Arrays y su método sort
+
+        Arrays.sort(arregloStrin);
+
+        //Pasar el contenido de un String a un variable intermedia
+
+        String arrString0 = arregloStrin[0];
+        String arrString1 = arregloStrin[1];
+        String arrString2 = arregloStrin[2];
+        String arrString3 = arregloStrin[3];
+        String arrString4 = arregloStrin[4];
+        String arrString5 = arregloStrin[5];
+        String arrString6 = arregloStrin[6];
+
+
+
+        //Imprimiendo la asignación de la variable intermedia
+
+        System.out.println("arrString0 = " + arrString0);
+        System.out.println("arrString1 = " + arrString1);
+        System.out.println("arrString2 = " + arrString2);
+        System.out.println("arrString3 = " + arrString3);
+        System.out.println("arrString4 = " + arrString4);
+        System.out.println("arrString5 = " + arrString5);
+        System.out.println("arrString6 = " + arrString6);
+
+
+
+
 
 
     }
