51a1798d0b6ace8a8d1fe42ef647c10906d5d853
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-10-04 20:07:50 -0500

    Agregamos la clase EjemploArrayForInverso

---
+++ src/Section11_Arreglos/EjemploArrayForInverso.java
@@ -0,0 +1,33 @@
+package Section11_Arreglos;
+
+import java.util.Arrays;
+
+public class EjemploArrayForInverso {
+    public static void main(String[] args) {
+
+        //Inicializamos un array de 7 espacios
+        String[] productos = new String[7];
+
+        //Asignamos valores a los espacios
+        productos[0] = "Cuchillo";
+        productos[1] = "Tenedor";
+        productos[2] = "Cuchara";
+        productos[3] = "Plato";
+        productos[4] = "Taza";
+        productos[5] = "Olla";
+        productos[6] = "Comal";
+
+        //Ordenamiento el arreglo
+        Arrays.sort(productos);
+
+        //Impresión inversa del arreglo
+        System.out.println("======= Ciclo For =======");
+
+        int total = productos.length;
+        for (int i = 0; i < total; i++){
+            System.out.println("Para el indice: " + (total -1 - i) +  "el valor es: " + productos[total - 1 - i]);
+
+        }
+
+    }
+}
