f2056ddef6036284c48c0ad542f9d2c56dd23e6a
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-10-21 20:23:19 -0500

    Agregamos la clase EjemploArrayEleminarElemento

---
+++ src/Section11_Arreglos/EjemploArrayEliminarElemento.java
@@ -0,0 +1,51 @@
+package Section11_Arreglos;
+
+import java.util.Scanner;
+
+public class EjemploArrayEliminarElemento {
+    public static void main(String[] args) {
+
+        //Iniciamos el array
+        int[] a = new int[10];
+
+        //Clase Scanner para llenar el array
+        Scanner s = new Scanner(System.in);
+
+        //Llenamos el array con un for
+        for (int i = 0; i < a.length; i++){
+            System.out.print("Ingrese un número: ");
+            a[i] = s.nextInt();
+        }
+
+        //Variable de control para eliminar un posicion
+        int posicion = 0;
+        System.out.println("Ingrese una posición para eliminar (entre 0 y 9): ");
+        posicion = s.nextInt();
+
+        //for para buscar la posición a eliminar
+        for (int i = posicion; i < a.length -1; i++){
+            a[i] = a[i + 1];
+        }
+
+        //a[a.length -1] = a[a.length - 2];
+        //Imprimimos el array
+        for (int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+
+        //Copiar un array con la clase System
+        //Definimos otro array
+        int[] b = new int[a.length -1];
+
+        //Clase system
+        System.arraycopy(a, 0, b, 0, b.length);
+
+        //Imprimimos
+        System.out.println("EL nuevo array es:");
+        for (int i = 0; i < b.length; i++){
+            System.out.println(b[i]);
+        }
+
+
+    }
+}
