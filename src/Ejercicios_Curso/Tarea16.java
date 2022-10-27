4dc6efe0b3971c1b091a1225e86d60503ae01302
DavidMecalco <66454464+DavidMecalco@users.noreply.github.com>
2022-10-27 14:22:08 -0500

    Agregamos la clase Tarea16 y Tarea17

---
+++ src/Ejercicios_Curso/Tarea16.java
@@ -0,0 +1,47 @@
+package Ejercicios_Curso;
+
+import java.util.Scanner;
+
+public class Tarea16 {
+    public static void main(String[] args) {
+
+        //Para la tarea se debe crear un arreglo con 10 elementos (enteros en el rango de 1 a 9).
+        //Escriba un programa que imprima el número que tiene más ocurrencias en el arreglo y también imprimir
+        //la cantidad de veces que aparece en el arreglo.
+
+        //Por ejemplo, para el arreglo: {1, 2, 3, 3, 4, 5, 5, 5, 6, 7}
+
+        //Inicializamos el array
+        int[] numeros = new int[10];
+
+        //Clase Scanner para ingresar los datos al array
+        Scanner s = new Scanner(System.in);
+
+        //Ciclo for para poblar el array
+        for(int i = 0; i < numeros.length; i++){
+            System.out.print("Ingrese un número: ");
+            numeros[i] = s.nextInt();
+        }
+
+        //Imprimos el array
+        System.out.println("El arreglo es: ");
+        for (int i = 0; i < numeros.length; i++){
+            System.out.print(numeros[i] + "\t");
+        }
+
+        System.out.println();
+        //Algoritmo para imprimir la concurrencia y la cantidad de veces que aparece
+        int concurrencia = 0, vecesrepetido = 0;
+
+        for (int i = 0; i < numeros.length - 1; i++){
+            if (numeros[i] == numeros[i + 1]){
+                concurrencia++;
+                vecesrepetido = numeros[i];
+            }
+        }
+
+        System.out.println("La mayor ocurrencias es: " + concurrencia);
+        System.out.println("El elemento que mas se repite es: " + vecesrepetido);
+
+    }
+}
---
+++ src/Ejercicios_Curso/Tarea17.java
@@ -0,0 +1,48 @@
+package Ejercicios_Curso;
+
+import java.util.Scanner;
+
+public class Tarea17 {
+    public static void main(String[] args) {
+
+        //Inicializamos el array
+        int[] histograma = new int[12];
+        //Clase Scanner
+        Scanner s = new Scanner(System.in);
+        //Ciclo for para recorrer el array y poblarlo
+        for(int i = 0; i < histograma.length; i++){
+            System.out.print("Ingrese un número entre 1 y 6: ");
+            histograma[i] = s.nextInt();
+        }
+        System.out.println();
+        //Mostramos el histograma
+        for (int i = 0; i < histograma.length; i++){
+            System.out.print(histograma[i] + "\t");
+        }
+        System.out.println();
+        //Creamos otro Arreglo para mostrar el índice que indica el ejemplo;
+        String[] indiceHistograma = new String[6];
+        //Ciclo para iterar el array de Strings
+        for (int i = 0; i < indiceHistograma.length; i++){
+            //Contador para mostrar el índice de la gráfica
+            int formato = i + 1;
+            //Guardamos el índice de 1 a 6
+            String grafica = formato + ": ";
+            //Iteramos el primer array y sumamos el string * a la cadena del segundo array
+            for (int j = 0; j < histograma.length; j++){
+                if(formato == histograma[j]){
+                    //Sí coincide el indice de la iteracion y el de arreglo de suma un * al array de String
+                    grafica += " *";
+                }
+            }
+            //Se asigna la concatenación de String a índice en turno del ciclo
+            indiceHistograma[i] = grafica;
+        }
+        //Mostramos la gráfica
+        for (int i = 0; i < indiceHistograma.length; i++){
+            //Mostramos la impresion del segundo array
+            System.out.println(indiceHistograma[i]);
+        }
+
+    }
+}
